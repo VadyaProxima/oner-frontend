81675b966968eee34527c8d28d569b9b4e1a8221
Vadim Lyashko <vadim.liaschko123gmail.com>
2024-05-27 18:18:01 +0300

    another one fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import Image from "next/image";
-import Header from "@/Components/Header/Header";
-import HeaderMenu from "@/Components/HeaderMenu";
 import PromotionSlider from "@/Components/Promo";
-import CategoryMenu from "@/Components/CategoryMenu";
-import Card from "@/Components/Catalog/Card";
 import Catalog from "@/Components/Catalog";
 export default function Home() {
   return (
